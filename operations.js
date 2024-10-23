eb9a772e2c1dc16ea4a68892171ee717da3bb91d
Yezen Armout <58712447+yarmout@users.noreply.github.com>
2024-10-23 14:41:06 -0400

    Add basic arithmetic operations and operate function
    Implement functions for addition, subtraction, multiplication,
    and division. Introduce the operate function to perform
    calculations based on the selected operator. This lays the
    foundation for further development of a calculator application.

---
+++ operations.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+    }
+}
