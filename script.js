6c898b9cc8ae00b90a61241c703290fb23c1c088
Yezen Armout <58712447+yarmout@users.noreply.github.com>
2024-11-05 16:34:13 -0500

    feat: Round long decimal results to prevent display overflow
    - Added `roundResult()` function to limit result length to 10 digits, accommodating both whole and decimal numbers.
    - Integrated `roundResult()` within the equals button event listener to round results before updating the display.
    - Ensures results fit within display limits, enhancing usability by preventing overflow with lengthy decimals.

--- script.js
+++ script.js
@@ -36,6 +36,18 @@ const clearDisplay = () => {
     updateDisplay('0');
 }
 
+// Round the decimals of the result to fit within the display
+const roundResult = (result) => {
+    const totalNumberOfDigits = result.toString().replace('.', '').replace('-', '').length;
+    const numberOfDigitsBeforeDecimal = Math.abs(Math.trunc(result)).toString().length;
+
+    if (totalNumberOfDigits > 10 && numberOfDigitsBeforeDecimal < 10) {
+        // Shorten length of number to 10 digits by rounding the decimal
+        result = result.toFixed(10 - numberOfDigitsBeforeDecimal);
+    }
+    return result;
+}
+
 // Number button event listeners
 numpad.forEach(numberButton => {
     numberButton.addEventListener("click", () => {
@@ -71,6 +83,8 @@ equalsBtn.addEventListener("click", () => {
     if (firstNumber === null || secondNumber === null || operator === null) return;
 
     const result = operate(firstNumber, secondNumber, operator);
+    let roundedResult = roundResult(result);
     clearOnNextInput = true;
-    updateDisplay(result);
+    updateDisplay(roundedResult);
 });
+
